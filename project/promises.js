cbac41a1c1f313f8f02aa13a6ae230b82600fd27
Acoustic6 <dorozhko.dmitry@gmail.com>
2017-08-15 22:55:50 +0300

    need callback

--- project/promises.js
+++ project/promises.js
@@ -1,1 +1,8 @@
-console.log('initial');
+function applyForVisa(documente) {
+    setTimeout(function() {
+        let visa = {};
+        return visa;//we want to return visa but we cant do it like that. to solve it we need to use  a callback
+    }, 2000);
+}
+
+applyForVisa({});
