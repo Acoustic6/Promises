d3600f6b2e5d2897e35b7b4ff420bd55bd09536a
Acoustic6 <dorozhko.dmitry@gmail.com>
2017-08-22 21:19:41 +0300

    chaining

--- project/promises.js
+++ project/promises.js
@@ -2,24 +2,29 @@ function applyForVisa(document) {
     console.log('обработка заявления');
     let promise = new Promise(function(resolve, reject) {
         setTimeout(function() {
-            Math.random() > .5 ? resolve({}) : reject('отказ. не достаточног документов.');
+            resolve({});
         }, 2000);
     });
     return promise;
 }
 
 applyForVisa({})
-    .then(visa => console.info('visa granted'))
-    .then(bookHotel)
+    .then(visa => {
+        console.info('visa granted');
+        return visa;
+    })
+    .then(bookHotel)// i pass function as an object as a resolve argument. visa goes there automatically.
     .then(byTickets)
-    .catch(error => console.log('error'))
-    .then(console.log('i am still here'));
+    .catch(error => console.log('error'));
 
-function bookHotel() {
-
+function bookHotel(visa) {
+    console.info('booking');
+    console.log('visa: ' + visa);
+    return visa;// if i dont return byTickets wont get visa
 };
 
-function byTickets() {
-
+function byTickets(visa) { //if frxmpl a dont specify argument here we will see 'error' from catch.
+    console.info('buying');
+    console.log('visa: ' + visa);
 };
 
